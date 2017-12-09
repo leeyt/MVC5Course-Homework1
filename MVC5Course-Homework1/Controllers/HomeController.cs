b12f40d72dc841ebfbf4413dac72aa6ddb9de6f8
Neil Lee <neil.lee@udngroup.com.tw>
2017-12-09 20:27:35 +0800

    新增首頁，並於主選單加入可以直接連到三個主要功能列表頁的連結

---
+++ MVC5Course-Homework1/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC5Course_Homework1.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
