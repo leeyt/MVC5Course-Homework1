da5e123bb0130c55d704913c0efcb0a883890c04
Neil Lee <neil.lee@udngroup.com.tw>
2017-12-15 20:31:07 +0800

    調整客戶資料的所有頁面，加上「客戶分類」的顯示

--- MVC5Course-Homework1/Controllers/客戶資料Controller.cs
+++ MVC5Course-Homework1/Controllers/客戶資料Controller.cs
@@ -90,7 +90,7 @@
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
+        public ActionResult Edit([Bind(Include = "Id,客戶分類,客戶名稱,統一編號,電話,傳真,地址,Email")] 客戶資料 客戶資料)
         {
             if (ModelState.IsValid)
             {
